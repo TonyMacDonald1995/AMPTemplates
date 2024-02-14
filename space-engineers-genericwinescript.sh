8e5ba0bd8527afdf03bcd19292be16ce455a274c
TonyMacDonald1995 <42494088+TonyMacDonald1995@users.noreply.github.com>
2024-02-14 05:31:06 -0600

    Update space-engineers-genericwinescript.sh

--- space-engineers-genericwinescript.sh
+++ space-engineers-genericwinescript.sh
@@ -16,15 +16,13 @@ wget -N https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winet
 chmod +x winetricks
 
 export WINEPREFIX="$SCRIPTDIR/space-engineers-generic/.wine"
-export WINEDLLOVERRIDES="mscoree,mshtml="
 export WINEARCH=win64
-export WINEDEBUG=fixme-all
-export DISPLAY=:$DPY_NUM
-./winetricks corefonts > winescript_log.txt 2>&1
+export WINEDEBUG=-all
+wine winecfg /v win10 > winescript_log.txt 2>&1
+./winetricks corefonts >> winescript_log.txt 2>&1
 ./winetricks sound=disabled >> winescript_log.txt 2>&1
-./winetricks -q vcrun2013 >> winescript_log.txt 2>&1
-./winetricks -q vcrun2019 >> winescript_log.txt 2>&1
-./winetricks -q dotnet48 >> winescript_log.txt 2>&1
+DISPLAY=:$DPY_NUM ./winetricks -q vcrun2019 >> winescript_log.txt 2>&1
+DISPLAY=:$DPY_NUM ./winetricks -q --force dotnet48 >> winescript_log.txt 2>&1
 rm -rf ~/.cache/winetricks ~/.cache/fontconfig
 
 exec 6>&-
